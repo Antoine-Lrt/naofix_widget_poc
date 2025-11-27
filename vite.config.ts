c269334b0175c3475497860d01bcef1173796fa9
AntoineLrt <antoinelaurentdev@gmail.com>
2025-11-27 10:18:38 +0100

    remove netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,16 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import viteReact from "@vitejs/plugin-react";
-import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
-  base: "/",
   server: {
     port: 3000,
   },
   build: {
     outDir: "dist",
-    sourcemap: true,
   },
   ssr: {
     noExternal: ["@mui/*"],
-  },
-  routers: {
-    ssr: {
-      entry: "./src/routes/__root.tsx",
-    },
   },
   plugins: [
     tsConfigPaths({
@@ -27,6 +19,5 @@ export default defineConfig({
     }),
     tanstackStart(),
     viteReact(),
-    netlify(),
   ],
 });
